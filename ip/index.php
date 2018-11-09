301f70a9385963f93d22b2f5068a90c4a3551907
phwt.smwt <phwt.smwt@gmail.com>
2018-11-09 15:00:47 +0700

    update

--- ip/index.php
+++ ip/index.php
@@ -7,14 +7,14 @@
     });
     function lookup(){
         if ($('#ipa').val() == ""){
-            cur_ip = "<?php echo $_SERVER['REMOTE_ADDR'] ?>";
+            url = "https://ipinfo.io/json";
         } else{
-            cur_ip = $('#ipa').val();
+            url = "https://ipinfo.io/"+$('#ipa').val();+"/json?token=2f252b1023a121";
         }
         $('#form').slideUp();$('#res').slideDown();
-        $.getJSON("https://ipinfo.io/"+cur_ip+"/json?token=2f252b1023a121", function(res){
+        $.getJSON(url, function(res){
             $('.b_ip').html(res.ip);
-            $('.b_city').html(res.city);
+            $('.b_city').html(if(res.city != "") res.city);
             $('.b_region').html(res.region);
             $('.b_country').html(res.country);
             $('.b_postal').html(res.postal);
@@ -29,14 +29,29 @@
         <input id="ipa" class="form-control" placeholder="Enter IP address here. Leave blank to use your current IP address."/><br>
         <button class="btn btn-success btn-lg btn-block" onclick="lookup()">Submit</button>
     </div>
-    <div class="card" id="res">
-        <div class="card-body">
-            <b>IP Address: </b><span class="b_ip">Undefined</span><br>
-            <b>City: </b><span class="b_city">Undefined</span><br>
-            <b>Region: </b><span class="b_region">Undefined</span><br>
-            <b>Postal Code: </b><span class="b_postal">Undefined</span><br>
-            <b>Country: </b><span class="b_country">Undefined</span><br>
-            <b>ISP: </b><span class="b_isp">Undefined</span>
-        </div>
+    <div id="res">
+        <div class="row">
+            <div class="col-md-6">
+                <iframe
+                    width="100%"
+                    frameborder="0" style="border:0"
+                    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDzndgrPuhUFE_xdJmblexiYHsXWLCFyzE
+                    &q=15.556433,103.276402" allowfullscreen>
+                </iframe>
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <b>IP Address: </b><span class="b_ip">Undefined</span><br>
+                        <b>City: </b><span class="b_city">Undefined</span><br>
+                        <b>Region: </b><span class="b_region">Undefined</span><br>
+                        <b>Postal Code: </b><span class="b_postal">Undefined</span><br>
+                        <b>Country: </b><span class="b_country">Undefined</span><br>
+                        <b>Origin: </b><span class="b_isp">Undefined</span>
+                    </div>
+                </div>
+            </div>
+        </div><br>
+        <button class="btn btn-primary btn-lg btn-block" onclick="location.reload()">Start Over</button>
     </div>
 </div>
